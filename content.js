e449504e614fa62c3c8abbdd74de8480bb1fd7ec
Ilian Azzouz <ilian.azzouz@stoik.io>
2025-01-20 17:55:22 +0100

    Add action to go to specefic ID & add copy/goto succes or goto fail

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
 // General URL = https://docs.google.com/presentation/d/15t24M-o_PGMFdkZ-Q6xbPucWJMXKGHyVrUYWkTCnyEI/edit#slide=id.g2ebc14ed5c9_0_221
-
 const options = [
   {
     id: "copy-document-id",
@@ -10,6 +9,7 @@ const options = [
       const id = url.match(/d\/(.*)\/edit/)[1];
       navigator.clipboard.writeText(id);
       console.log(`Document ID copied: ${id}`);
+      createAlert(`Document ID copied: ${id}`);
     }
   },
   {
@@ -21,10 +21,67 @@ const options = [
       const id = url.match(/slide=id\.(.*)/)[1];
       navigator.clipboard.writeText(id);
       console.log(`Slide ID copied: ${id}`);
+      createAlert(`Slide ID copied: ${id}`);
+    }
+  },
+  {
+    id: "goto-slide-id",
+    name: "Go to copied slide id",
+    icon: "docs-icon-editors-ia-view-show",
+    action: () => {
+      const copied = navigator.clipboard.readText();
+      copied.then(id => {
+        const url = window.location.href;
+        const newUrl = url.replace(/slide=id\..*/, `slide=id.${id}`);
+        window.location.href = newUrl;
+
+        // Check if the slide id is valid
+        const escapedId = CSS.escape(id);
+        const slide = document.querySelector(`[id$="${escapedId}"]`);
+        if (slide) {
+          console.log(`Navigating to slide id: ${id}`);
+          createAlert(`Navigating to slide id: ${id}`);
+        } else {
+          console.log(`Invalid slide id: ${id}`);
+          createAlert(`Invalid slide id: ${id}`, false);
+        }
+      })
     }
   }
 ]
 
+/**
+ * 
+ * @param {string} message Message to display
+ * @param {boolean} success If the alert is a success or error, default is success
+ */
+
+const createAlert = (message, success = true) => {
+  const alert = document.createElement("div");
+  alert.style.position = "fixed";
+  alert.style.top = "10px";
+  alert.style.left = "10px";
+  alert.style.padding = "10px";
+  alert.style.borderRadius = "5px";
+  alert.style.backgroundColor = "#333";
+  alert.style.color = "#fff";
+  alert.style.transition = "opacity 0.5s";
+  alert.style.zIndex = "99999";
+  alert.className = "custom-alert";
+
+  alert.innerHTML = `
+    <span>${success ? "✅" : "❌"} ${message}</span>
+  `;
+
+  document.body.appendChild(alert);
+
+  setTimeout(() => {
+    alert.style.opacity = 0;
+    setTimeout(() => {
+      alert.remove();
+    }, 500);
+  }, 2000);
+}
 
 document.addEventListener("click", () => {
   const menus = document.querySelectorAll(".apps-menu-hide-mnemonics");
@@ -32,7 +89,7 @@ document.addEventListener("click", () => {
   menus.forEach(menu => {
     if (!menu.querySelector(".custom-menu-item")) {
 
-      options.forEach(({id, name, icon, action}) => {
+      options.forEach(({id, name, icon, action, disabled}) => {
         const customItem = document.createElement("div");
         customItem.className = `goog-menuitem apps-menuitem custom-menu-item custom-menu-item-${id}`;
         customItem.role = "menuitem";
@@ -49,18 +106,24 @@ document.addEventListener("click", () => {
           </div>
         `;
 
-        customItem.addEventListener("click", () => {
-          action();
-          menu.style.display = "none";
-        });
+        if (disabled) {
+          customItem.classList.add("goog-menuitem-disabled");
+          customItem.tooltip = "Disabled";
+        } else {
+          customItem.addEventListener("click", () => {
+            action();
+            menu.style.display = "none";
+          });
 
-        customItem.addEventListener("mouseover", () => {
-          customItem.classList.add("goog-menuitem-highlight");
-        });
 
-        customItem.addEventListener("mouseout", () => {
-          customItem.classList.remove("goog-menuitem-highlight");
-        })
+          customItem.addEventListener("mouseover", () => {
+            customItem.classList.add("goog-menuitem-highlight");
+          });
+
+          customItem.addEventListener("mouseout", () => {
+            customItem.classList.remove("goog-menuitem-highlight");
+          });
+        }
 
         menu.appendChild(customItem);
 
